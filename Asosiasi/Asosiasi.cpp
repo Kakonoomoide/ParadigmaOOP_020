38d922d95042a237d6f0bef4c01f58b174e208bb
Kakonoomoide <rizkiramada128@gmail.com>
2023-05-24 17:35:58 +0700

    menambahkan construktor dan destruktor ke dalam class pasien

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -7,6 +7,14 @@ class pasien {
 public:
 	string nama;
 	vector<dokter*> daftar_dokter;
+
+	pasien(string pNama) :nama(pNama) {
+		cout << "pasien \"" << nama << "\" ada\n";
+	}
+
+	~pasien() {
+		cout << "pasien \"" << nama << "\" tidak ada\n";
+	}
 	
 	void tambahDokter(dokter*);
 	void cetakDokter();
