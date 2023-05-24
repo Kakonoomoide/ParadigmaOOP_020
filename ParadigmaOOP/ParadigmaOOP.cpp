f744f9b92dd37379d9284cc2772ead5a654f7869
Kakonoomoide <rizkiramada128@gmail.com>
2023-05-24 17:29:12 +0700

    membuat class pelajar dengan turunan dari class orang

--- ParadigmaOOP/ParadigmaOOP.cpp
+++ ParadigmaOOP/ParadigmaOOP.cpp
@@ -20,6 +20,26 @@ public:
 
 };
 
+class pelajar : public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pSekolah) :
+		orang(pNama),
+		sekolah(pSekolah) {
+		cout << "Pelajar Dibuat\n" << endl;
+	}
+
+	~pelajar() {
+		cout << "Pelajar Dihapus\n" << endl;
+	}
+
+	string perkenalan() {
+		return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n\n";
+	}
+
+};
+
 int main(){
 
 }
