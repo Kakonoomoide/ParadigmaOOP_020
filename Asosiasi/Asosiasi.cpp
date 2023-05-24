142143ddfb4dacb11df59197d8493e63e1df1974
Kakonoomoide <rizkiramada128@gmail.com>
2023-05-24 17:39:02 +0700

    menambahkan methode untuk menambah pasien

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -48,6 +48,11 @@ void pasien::cetakDokter() {
 	cout << endl;
 }
 
+void dokter::tambahPasien(pasien* pPasien) {
+	daftar_pasien.push_back(pPasien);
+	pPasien->tambahDokter(this);
+}
+
 int main() {
 
 }
